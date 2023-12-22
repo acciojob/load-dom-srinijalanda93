cc3e219bc5ef54bd75fdafa2b30ce8b78569b956
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2023-12-22 11:06:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-	//create an element
-	funtion write(){
-	const ptag=document.CreateElement("p");
-		//insert the content
-		ptag.innerText= "DOM load success";
-		//append to the body
-		document.body.appendChild(paragraph);
-	}
-	//call the function
-	write();
-	
-	
-})
+    // Create an element
+    function write() {
+        const ptag = document.createElement("p");
+        
+        // Insert the content
+        ptag.innerText = "DOM load success";
+        
+        // Append to the body
+        document.body.appendChild(ptag); // Use ptag instead of paragraph
+    }
+
+    // Call the function
+    write();
+});
