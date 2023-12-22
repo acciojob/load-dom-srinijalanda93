4893610117c9412b12a573b2b234a5405ab47abb
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2023-12-22 11:01:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+	//create an element
+	funtion write(){
+	const ptag=document.CreateElement("p");
+		//insert the content
+		ptag.innerText= "DOM load success";
+		//append to the body
+		document.body.appendChild(paragraph);
+	}
+	//call the function
+	write();
+	
+	
+})
